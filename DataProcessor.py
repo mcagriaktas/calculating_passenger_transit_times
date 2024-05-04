d41e424551ca579ba45cab6efaba3d6db1243164
Cagri <52080028+mcagriaktas@users.noreply.github.com>
2024-05-04 20:29:39 +0300

    Update DataProcessor.py

--- DataProcessor.py
+++ DataProcessor.py
@@ -1,124 +1,123 @@
-# Data manipulation modules
+# Datamanipulations modules
 import pandas as pd
+import json
+
+# Database modules
+import pymongo
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import text
 
 # Debugging modules
 import logging
 
-class DataProcessor:
-    def __init__(self):
-        pass
+class DatabaseController:
+    def __init__(self, mongo_ip, mongo_port, mongo_authSource, mongo_username, mongo_password, mongo_database,
+                 postgres_ip, postgres_port, postgres_database, postgres_username, postgres_password):
         
-    def check_left_area(self, row, vertices):
-        x, y = self.parse_position(row["POSITION"])
-        inside = self.is_point_in_polygon(x, y, vertices)
-        return "in" if inside else "out"
-    
-    @staticmethod
-    def parse_position(position):
-        if isinstance(position, str):
-            position_dict = eval(position)
-        elif isinstance(position, dict):
-            position_dict = position
-        else:
-            raise ValueError("POSITION data is not string nor dictionary")
-        return (float(position_dict["X"]), float(position_dict["Y"]))
-
-    @staticmethod
-    def is_point_in_polygon(px, py, vertices):
-        n = len(vertices)
-        inside = False
-        xinters = 0
-        p1x, p1y = vertices[0]
-        for i in range(n + 1):
-            p2x, p2y = vertices[i % n]
-            if py > min(p1y, p2y):
-                if py <= max(p1y, p2y):
-                    if px <= max(p1x, p2x):
-                        if p1y != p2y:
-                            xinters = (py - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
-                        if p1x == p2x or px <= xinters:
-                            inside = not inside
-            p1x, p1y = p2x, p2y
-        return inside
-    
-    def calculate_first_last_seen(self, df, vertices_list):
-        if df.empty:
-            logging.warning("No data fetched from MongoDB. Exiting processing.")
-            return pd.DataFrame()
+        self.mongo_ip = mongo_ip
+        self.mongo_port = mongo_port
+        self.mongo_authSource = mongo_authSource
+        self.mongo_username = mongo_username
+        self.mongo_password = mongo_password
+        self.mongo_database = mongo_database
         
-        logging.info(f"Starting data processing on DataFrame with {len(df)} rows.")
-        area_count = len(vertices_list)
-        logging.info(f"Total number of areas to process: {area_count}.")
+        self.postgres_ip = postgres_ip
+        self.postgres_port = postgres_port
+        self.postgres_database = postgres_database
+        self.postgres_username = postgres_username
+        self.postgres_password = postgres_password
         
-        df["col2"] = pd.to_datetime(df["col2"], unit="s")
-        df["WINDOW"] = pd.to_datetime(df["col2"], unit="s")
-        
-        result_df = pd.DataFrame(df['col1'].unique(), columns=['col1'])
+        self.mongo_client = None
+        self.mongo_db = None
+        self.mongo_collection = None
+        self.postgres_engine = None
 
-        main_first_seen = df.groupby('col1')['WINDOW'].min().to_frame(name='main_first_seen')
-        main_last_seen = df.groupby('col1')['WINDOW'].max().to_frame(name='main_last_seen')
-        main_total = ((main_last_seen['main_last_seen'] - main_first_seen['main_first_seen'])
-                      .dt.total_seconds().div(60).round().astype('Int64').to_frame(name='main_total'))
-        
-        result_df = result_df.merge(main_first_seen, on='col1', how='left')
-        result_df = result_df.merge(main_last_seen, on='col1', how='left')
-        result_df = result_df.merge(main_total, on='col1', how='left')
+    def connect_to_mongodb(self, collection_name):
+        try: 
+            self.mongo_client = pymongo.MongoClient(
+                host=f"mongodb://{self.mongo_ip}:{self.mongo_port}/?authSource={self.mongo_authSource}", 
+                username=self.mongo_username, 
+                password=self.mongo_password)
+            self.mongo_db = self.mongo_client[self.mongo_database]
+            self.mongo_collection = self.mongo_db[collection_name]
+            logging.info(f"Connected mongo database: {self.mongo_database} and collection: {collection_name}.")
+        except Exception as e:
+            logging.error(f"Failed to connect to MongoDB: {e}")
 
-        for index, vertices in enumerate(vertices_list, start=1):
-            area_name = f'area{index}'
-            
-            df[area_name + '_in'] = df.apply(lambda row: self.check_left_area(row, vertices), axis=1)
-            
-            area_in_df = df[df[area_name + '_in'] == 'in']
-            first_seen = area_in_df.groupby('col1')['WINDOW'].min().to_frame(name=f'{area_name}_first_seen')
-            last_seen = area_in_df.groupby('col1')['WINDOW'].max().to_frame(name=f'{area_name}_last_seen')
-            area_total_time = (area_in_df.groupby('col1').size().multiply(30).div(60)
-                            .round().astype('Int64').to_frame(name=f'{area_name}_total'))
+    def fetch_data_from_mongo(self, start_datetime=None, end_datetime=None):
+        query = {}
+        if start_datetime and end_datetime:
+            start_timestamp = int(start_datetime.timestamp())
+            end_timestamp = int(end_datetime.timestamp())
+            query['WINDOW_START'] = {'$gte': start_timestamp, '$lte': end_timestamp}
+        fields = {"CLIMAC": 1, "WINDOW_START": 1, "POSITION": 1}
+        x = self.mongo_collection.find(query, fields)
+        df = pd.DataFrame(list(x))
+        return df
 
-            result_df = result_df.merge(first_seen, on='col1', how='left')
-            result_df = result_df.merge(last_seen, on='col1', how='left')
-            result_df = result_df.merge(area_total_time, on='col1', how='left')
+    def fetch_from_postgres(self, table_name, columns):
+        """
+        Fetches data from a PostgreSQL table, handling multiple columns and truncating datetime fields.
 
-        result_df = result_df.where(pd.notnull(result_df), None)
-        logging.info(f"Data processing completed for all areas.")
+        Args:
+        table_name (str): The name of the PostgreSQL table to fetch data from.
+        columns (list): List of column names to fetch, with datetime truncation applied if needed.
 
-        return result_df
-        
-    def calculate_transitions_between_areas(self, df, vertices_list):
-        if df.empty:
-            logging.warning("No data fetched from MongoDB. Exiting processing.")
-            return pd.DataFrame()
+        Returns:
+        DataFrame: A pandas DataFrame containing the fetched data.
+        """
+        trunc_columns = []
+        for column in columns:
+            if 'first_seen' in column:
+                trunc_columns.append(f"DATE_TRUNC('hour', {column}) AS {column}")
+        query = f"SELECT {', '.join(trunc_columns)} FROM {table_name}"
+        return pd.read_sql(query, self.postgres_engine)
 
-        df["col2"] = pd.to_datetime(df["col2"], unit="s")
-        
-        result_df = pd.DataFrame(df['col1']. unique(), columns=['col1'])
-        
-        for index, vertices in enumerate(vertices_list, start=1):
-            area_name = f'area{index}'
-            df[area_name + '_in'] = df.apply(lambda row: self.check_left_area(row, vertices), axis=1)
-            
-            area_in_df = df[df[area_name + '_in'] == 'in']
-            first_seen = area_in_df.groupby('col1')['col2'].min().to_frame(name=f'{area_name}_first_seen')
-            last_seen = area_in_df.groupby('col1')['col2'].max().to_frame(name=f'{area_name}_last_seen')
-            area_total_time = (area_in_df.groupby('col1').size().multiply(30).div(60)
-                            .round().astype('Int64').to_frame(name=f'{area_name}_total'))
-            
-            result_df = result_df.merge(first_seen, on='col1', how='left')
-            result_df = result_df.merge(last_seen, on='col1', how='left')
-            result_df = result_df.merge(area_total_time, on='col1', how='left')
+    def connect_to_postgres(self):
+        try:
+            db_connection_str = f"postgresql://{self.postgres_username}:{self.postgres_password}@{self.postgres_ip}:{self.postgres_port}/{self.postgres_database}"
+            self.postgres_engine = create_engine(db_connection_str)
+            logging.info(f"Connected to PostgreSQL database: {self.postgres_database}")
+        except Exception as e:
+            logging.error(f"Failed to connected to PostgreSQL database: {self.postgres_database}")
 
-        valid_sequence_conditions = [result_df[f'area{1}_first_seen'].notnull()] 
-        for i in range(1, len(vertices_list)):
-            current_area = f'area{i}_first_seen'
-            next_area = f'area{i+1}_first_seen'
-            condition = result_df[current_area].notnull() & result_df[next_area].notnull() & (result_df[current_area] < result_df[next_area])
-            valid_sequence_conditions.append(condition)
+    def get_coordinates(self, area_ids):
+        with self.postgres_engine.connect().execution_options(stream_results=True) as connection:
+            sql_query = text("SELECT id, cordinate FROM area WHERE id = ANY(:area_ids)")
+            result = connection.execute(sql_query, {'area_ids': area_ids})
+            coordinates_map = {}
+            for row in result.mappings():
+                area_id = row['id']
+                json_data = row['cordinate']
+                if isinstance(json_data, str):
+                    coordinates = json.loads(json_data)  
+                else:
+                    coordinates = json_data 
+                formatted_coordinates = [(float(coord['lat']), float(coord['lng'])) for coord in coordinates]
+                coordinates_map[area_id] = formatted_coordinates
+            return coordinates_map
 
-        if valid_sequence_conditions:
-            valid_sequence = valid_sequence_conditions[0]
-            for condition in valid_sequence_conditions[1:]:
-                valid_sequence &= condition
+    def write_to_postgres(self, df, table_name):
+        df.columns = [c.lower() for c in df.columns]  
+        with self.postgres_engine.connect() as connection:
+            if not connection.dialect.has_table(connection, table_name):
+                df.to_sql(table_name, self.postgres_engine, index=False, if_exists='append')
+            else:
+                metadata = sqlalchemy.MetaData()
+                table = sqlalchemy.Table(table_name, metadata, autoload_with=self.postgres_engine)
+                existing_columns = [c.name for c in table.columns]
+                new_columns = [col for col in df.columns if col not in existing_columns]
 
-            result_df = result_df[valid_sequence]
+                for column in new_columns:
+                    col_type = sqlalchemy.String()  
+                    if 'first_seen' in column or 'last_seen' in column:
+                        col_type = sqlalchemy.DateTime()
+                    elif 'total' in column:
+                        col_type = sqlalchemy.Integer()
 
-        return result_df
+                    alter_cmd = sqlalchemy.schema.AddColumn(table_name, sqlalchemy.Column(column, col_type))
+                    connection.execute(alter_cmd)
+
+                df.to_sql(table_name, self.postgres_engine, index=False, if_exists='append', method='multi')
+            logging.info(f"Data written to Postgres successfully in the table: {table_name}")
